846ecca4d2512474fb8d2d61eb133494ffb512af
Prathmesh <prathmeshvairale17@gmail.com>
2026-02-09 14:36:48 +0530

    Improve embeddings by adding metadata

--- build_index.py
+++ build_index.py
@@ -12,9 +12,16 @@ from langchain.embeddings import HuggingFaceEmbeddings
 # Reading the Dataset from the folder
 try:
     df = pd.read_csv("dataset/preprocessed_data.csv")
+    # df = df.head(50) #For checking if this is working or not
     docs = [
         Document(
-            page_content=row['summary'],
+            page_content=f"""
+            Title: {row['title']}
+            Author: {row['author']}
+            Publish Year: {row['publish_year']}
+            Genres: {row['genres']}
+            Summary: {row['summary']}
+            """.strip(),
             metadata={
                 "title": row['title'],
                 "author": row['author'],
@@ -25,7 +32,10 @@ try:
         for _, row in df.iterrows()
     ]
 
-    embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    # embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    embedding_model = HuggingFaceEmbeddings(
+        model_name="BAAI/bge-small-en-v1.5"
+    )
     vectorstore = FAISS.from_documents(docs, embedding_model)
     vectorstore.save_local("index/faiss_index")
     print("___________________________________________________________")
